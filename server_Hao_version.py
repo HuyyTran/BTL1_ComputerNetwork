5645eda1c3b963a2582a90c5f90d8be0d6ef3218
binhow612 <91769295+binhow612@users.noreply.github.com>
2023-11-19 22:07:47 +0700

    Update server_Hao_version.py
    update lại server theo Huy

--- server_Hao_version.py
+++ server_Hao_version.py
@@ -32,7 +32,6 @@ class ServerCmd(cmd.Cmd):
             threading.Thread(target=self.handle_client, args=(client_conn, client_addr), daemon=True).start()
 
 
-
     def handle_client(self, client_conn, client_addr):
         while True:
             try:
@@ -85,18 +84,61 @@ class ServerCmd(cmd.Cmd):
         print(f"Client {client_addr} has disconnected")
         client_conn.close()
 
-    def do_discover(self, hostname):
-        "Discover the list of local files of the host named hostname"
-        # Use the ping command to check if the hostname exists and is connected
-        if self.do_ping(hostname, check_mode=True):  # Added check_mode parameter to ping method
-            # If the client is active, proceed to list the shared files
-            client_files = self.clients[hostname]['files']
-            print(f"Files shared by {hostname}: {client_files}")
-        else:
-            print(f"Hostname {hostname} is not active or does not exist.")
+    # def do_discover(self, hostname):
+    #     "Discover the list of local files of the host named hostname"
+    #     # Use the ping command to check if the hostname exists and is connected
+    #     if self.do_ping(hostname, check_mode=True):  # Added check_mode parameter to ping method
+    #         # If the client is active, proceed to list the shared files
+    #         client_files = self.clients[hostname]['files']
+    #         print(f"Files shared by {hostname}: {client_files}")
+    #     else:
+    #         print(f"Hostname {hostname} is not active or does not exist.")
+    
+    def parse_dictionary_s(self,contents):
+        files_dictionary = {}
+        lines = contents.strip().splitlines()
+        for line in lines:
+        # Remove the angle brackets and extra single quotes
+            parts = line.translate(str.maketrans('', '', "<>'")).split()
+            if len(parts) == 3:
+                address, port, file_name = parts
+                port = int(port)  # Convert the port to an integer
+                key = (address, port)
+                if key not in files_dictionary:
+                    files_dictionary[key] = []
+                files_dictionary[key].append(file_name)
+        return files_dictionary
+
+    def load_shared_files_dictionary(self):
+        # Load and parse the contents of dictionary_s.txt
+        with open('D:/dictionary_s.txt', 'r') as file:
+            contents = file.read()
+        return self.parse_dictionary_s(contents)
+    
+    def do_discover(self, arg):
+        "Discover the list of local files of the host named by address and port"
+        # Load the shared files from the dictionary
+        files_dictionary = self.load_shared_files_dictionary()
+        
+        # Parse the argument to get the address and port
+        try:
+            address, port_str = arg.split(',')
+            port = int(port_str)
+            client_key = (address, port)
+            
+            # Find the files shared by the specified client
+            if client_key in files_dictionary:
+                files_list = files_dictionary[client_key]
+                print(f"Files shared by {client_key}: {files_list}")
+            else:
+                print(f"No files shared by {client_key} or client not connected.")
+        except ValueError:
+            print(f"Incorrect format for discover command. Expected format: discover <address>,<port>")
+
+    
     
     def do_ping(self, arg, check_mode=False):
-        "Check if a host is active: PING <IP address>,<Port>"
+        "Check if a host is active: PING <hostname>"
         try:
             # Split the argument by comma and strip any whitespace
             hostname, port_str = arg.split(',')
