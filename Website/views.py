78163df561f3a793a15ad246c6f6faf52fa996d7
binhow612 <91769295+binhow612@users.noreply.github.com>
2023-12-12 21:42:29 +0700

    Update views.py hạo add

--- Website/views.py
+++ Website/views.py
@@ -13,3 +13,51 @@ def server_view():
 @views.route('/client')
 def client_view():
     return render_template('client.html')
+
+@views.route('/connect_server', methods=['GET', 'POST'])
+def connect():
+    global client
+    if request.method == "POST":
+        try:
+            ip = request.form['serverIp']
+            port = request.form['serverPort']
+            client = client_real.ClientShell(ip, int(port))
+            
+            # Create threads for the P2P client functions
+            Server_thread = threading.Thread(target=client_real.run_Server)
+            # Start threads
+            Server_thread.start()
+            if client:
+                return render_template('client.html', message=f"Connect Successfully for {ip} and {port} server!")
+        except Exception as e:
+            return render_template('client.html', message=f"Error: {e}")
+    else:
+        return render_template('client.html', message='')
+
+
+@views.route('/publish', methods=['POST'])
+def publish():
+    if request.method == "POST":
+        try:
+            lname = request.form['lname']
+            fname = request.form['fname']
+            filepath = request.form['filepath']
+
+            # Use client_shell to handle publishing logic
+            response = client.do_publish(lname, fname, filepath)
+            
+        except Exception as e:
+            response = f"Error: {e}"
+            return render_template('client.html', message=response)
+    else:
+        pass
+
+
+@views.route('/fetch', methods=['POST'])
+def fetch():
+    fname = request.form['fname']
+
+    # Use client_shell to handle fetching logic
+    response = client.do_fetch(fname)
+
+    return response
